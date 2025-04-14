4c07c197bd20fe29bf5d4fbd8337f8552884766c
Christian Nathorst <jofel.nathorst@gmail.com>
2025-04-14 21:56:18 +0800

    Added BasePage and WebdriverSetup

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage{
+
+    @FindBy(linkText = "Sign In")
+    private WebElement signInIcon;
+
+    public HomePage(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void clickSignInIcon(){
+        click(signInIcon);
+    }
+}
---
+++ src/test/java/pages/SignInPage.java
@@ -0,0 +1,33 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SignInPage extends HomePage{
+
+    @FindBy(id = "email")
+    private WebElement emailField;
+    @FindBy(id = "password")
+    private WebElement passwordField;
+    @FindBy(css = "button[title='Sign in with Newspapers.com']")
+    private WebElement NewsPapersButton;
+
+    public SignInPage(WebDriver driver){
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterEmail(String email){
+        type(emailField, email);
+    }
+
+    public void enterPassword(String password){
+        type(passwordField, password);
+    }
+
+    public void signInWithNewspapersButton(){
+        click(NewsPapersButton);
+    }
+}
