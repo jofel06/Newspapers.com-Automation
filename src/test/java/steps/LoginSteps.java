290b496776127c52f6931eb5ce9550a2e1bcde91
Christian Nathorst <jofel.nathorst@gmail.com>
2025-04-21 17:17:10 +0800

    Add Parallel Testing using Testng. maven surefire plugin.

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,39 +1,34 @@
 package steps;
 
-import base.ScenarioContext;
+import base.Hooks;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import models.LoginDataModel;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import pages.HomePage;
 import pages.LoginPage;
 import utils.ConfigReader;
 import utils.LoginDataReader;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class LoginSteps{
 
-    private ScenarioContext scenarioContext;
     private LoginPage loginPage;
     private HomePage homePage;
-    private LoginDataModel loginDataModel;
-    private String baseURL;
+    LoginDataModel loginDataModel = LoginDataReader.getLoginData();
 
-    public LoginSteps(ScenarioContext scenarioContext){
-        System.out.println("LoginSteps constructor called with ScenarioContext: " + scenarioContext); // Add this
-        this.scenarioContext = scenarioContext;
-        this.loginDataModel = LoginDataReader.getLoginData();
-        this.baseURL = scenarioContext.getBaseURL();
-        System.out.println("LoginSteps constructor - baseURL: " + this.baseURL); // Add this
+    public LoginSteps(){
+        this.loginPage = new LoginPage(Hooks.getDriver());
+        this.homePage = new HomePage(Hooks.getDriver());
     }
 
     @Given("the user is on the home page")
     public void userOnHomePage(){
-        System.out.println("@Given userOnHomePage() - Getting Driver from ScenarioContext: " + scenarioContext.getDriver()); // Add this
-        this.homePage = new HomePage(scenarioContext.getDriver());
-        this.loginPage = new LoginPage(scenarioContext.getDriver());
-        scenarioContext.getBaseURL();
+       Hooks.getDriver().get(ConfigReader.getUrl());
     }
 
     @When("the user navigates to the Sign in page")
@@ -41,15 +36,11 @@ public class LoginSteps{
         homePage.clickSignInIcon();
     }
 
-    @And("the user logs in with valid data")
+    @Then("the user logs in with valid data")
     public void userEnterValidData(){
         loginPage.enterEmail(loginDataModel.getEmail());
         loginPage.enterPassword(loginDataModel.getPassword());
     }
 
-    @Then("the user should be redirected to the homepage")
-    public void userRedirectedToHomepage(){
-        String expectedUrl = baseURL;
-        Assert.assertEquals(scenarioContext.getDriver().getCurrentUrl(), expectedUrl, "User was not redirected to the homepage");
-    }
+
 }
