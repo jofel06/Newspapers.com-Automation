d8befa230979dc29e6c428a4953a860c44a74d52
Christian Nathorst <jofel.nathorst@gmail.com>
2025-04-30 17:07:07 +0800

    Confirmed that test also runs in other OS. E.g. Linux, change maven compiler to run in version 21

--- src/main/java/models/LoginDataModel.java
+++ src/main/java/models/LoginDataModel.java
@@ -12,13 +12,4 @@ public class LoginDataModel {
     public String getPassword(){
         return password;
     }
-
-    //Setters
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
